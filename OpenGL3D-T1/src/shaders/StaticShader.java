db332d71cbda034bc17305125249ac5fc6d36f42
emrecellebi <Emre_celebi_199505@hotmail.com>
2022-03-13 12:52:39 +0300

    Coloring using Shaders - Color Part 1

---
+++ OpenGL3D-T1/src/shaders/StaticShader.java
@@ -0,0 +1,18 @@
+package shaders;
+
+public class StaticShader extends ShaderProgram
+{
+	private static final String VERTEX_FILE = "res/shaders/vertexShader.txt";
+	private static final String FRAGMENT_FILE = "res/shaders/fragmentShader.txt";
+	
+	public StaticShader()
+	{
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+	
+	@Override
+	protected void bindAttributes()
+	{
+		super.bindAttribute(0, "position");
+	}
+}
