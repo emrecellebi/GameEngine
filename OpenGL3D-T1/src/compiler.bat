5660ff43c9bcdc9ce5910c2aaada4e637f93d8f3
emrecellebi <Emre_celebi_199505@hotmail.com>
2022-03-13 16:30:11 +0300

    Model, View & Projection Matrices

--- OpenGL3D-T1/src/compiler.bat
+++ OpenGL3D-T1/src/compiler.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set LWJGL=../lib/lwjgl.jar;../lib/jinput.jar;../lib/slick-util.jar;
+set LWJGL=../lib/lwjgl.jar;../lib/lwjgl_util.jar;../lib/slick-util.jar;
 set CLASS_PATH=-d ../bin -cp %LWJGL%
 
 javac.exe %CLASS_PATH% enginTester/MainGameLoop.java
--- OpenGL3D-T1/src/run.bat
+++ OpenGL3D-T1/src/run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set LWJGL=../lib/lwjgl.jar;../lib/jinput.jar;../lib/slick-util.jar;
+set LWJGL=../lib/lwjgl.jar;../lib/lwjgl_util.jar;../lib/slick-util.jar;
 set CLASS_PATH=-cp ../bin;%LWJGL%
 
 java.exe %CLASS_PATH% enginTester.MainGameLoop %*
